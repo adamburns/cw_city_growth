bded841167b8d540d1710ee720fab1da7499766d
Adam <burns679@umn.edu>
2019-05-20 08:57:31 -0500

    Removed non-essential data

--- Tax Revenue/revenue.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-URL = 'https://www2.census.gov/programs-surveys/qtax/tables/2018/q1t1.xls'
-with requests.get(URL) as response:
-    with open('revenue.xls', 'wb') as output:
-        output.write(response.content)
