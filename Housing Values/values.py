1e33755a2da0db5d50a02c7c82b34fb3c8e984b3
Adam <burns679@umn.edu>
2019-05-20 08:39:35 -0500

    Edited script to scrape all data from Zillow

--- Housing Values/values.py
+++ Housing Values/values.py
@@ -1,9 +1,26 @@
+from bs4 import BeautifulSoup
+from time import sleep
+import json
 import requests
 
-URL = 'http://files.zillowstatic.com/research/public/City/City_Zhvi_AllHomes.csv'
+URL = 'https://www.zillow.com/research/data/'
 
 with requests.get(URL) as response:
-    with open('City_Zhvi_AllHomes.csv', 'wb') as output:
-        output.write(response.content)
-
-
+    soup = BeautifulSoup(response.text, 'lxml')
+    soup = soup.findAll('script')
+    soup = soup[len(soup) - 25]
+    soup = str(soup).replace('\\', '')
+    soup = json.loads(soup[17:-10])
+    #Traverse over nested dictionary in the script to get the URLs
+    for dataType in soup:
+        for x in soup[dataType]:
+            for y in soup[dataType][x]:
+                temp_url = soup[dataType][x][y]
+                with requests.get(temp_url) as csv:
+                    #Split the string to get the filename in format X.csv or X.xlsx
+                    temp_name = temp_url.split('/')[-1]
+                    with open(temp_name, 'wb') as output:
+                        print(temp_name)
+                        output.write(csv.content)
+                        #time out to avoid DDOS
+                        sleep(5)
